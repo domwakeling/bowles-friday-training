461fd7ec8a28b0b7eaff53d14e3d7ae83e02a517
Dom Wakeling <domwakeling@gmail.com>
2022-09-17 14:45:34 +0100

    Extend to 45 racers for fun race

--- pages/api/booking/[week]/index.js
+++ pages/api/booking/[week]/index.js
@@ -52,7 +52,7 @@ handler.post(async (req, res) => {
   // eslint-disable-next-line max-len
   const racerFound = bookings ? bookings.racers.filter((racer) => (racer.userid === id && racer.name === name)).length > 0 : false;
 
-  const maxRacers = forFriday === '29042022' ? 40 : 25;
+  const maxRacers = forFriday === '07102022' ? 45 : 25;
 
   // if no booking was found set one up; expire after 30 days (minmimse db size)
   if (!bookings) {
